cce447c294601c7c602542c8cc143c7544786076
Alex12012000 <arcex.29@gmail.com>
2022-06-20 16:10:45 +0200

    Milestone 2

--- js/main.js
+++ js/main.js
@@ -38,3 +38,55 @@ const post = [
     }
 ]
 
+
+let postContainer = document.getElementById('container'); //Variabile che richiama il container dei post nel DOM
+drawToDom(post) //Invoco la variabile
+
+// ----------------
+//  FUNCTION
+// ----------------
+
+// Funzione che crea i post nel dom utilizzando l'array dei post
+function drawToDom (postArray) {
+
+    // Per ogni oggetto nell'array appendo nel container del DOM il template
+    for(let i = 0; i < post.length; i++){
+        let singleContent = post[i];
+        
+        // Template nel DOM
+        postContainer.innerHTML += `
+        <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${singleContent.profile} alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${singleContent.name}</div>
+                    <div class="post-meta__time">${singleContent.date}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${singleContent.content}</div>
+        <div class="post__image">
+            <img src=${singleContent.img} alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${singleContent.like}</b> persone
+                </div>
+            </div> 
+        </div>            
+        </div>
+        `;
+
+    }
+
+}
