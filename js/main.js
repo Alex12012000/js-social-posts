559f844b80628ed89af2369976003c25fca39ce1
Alex12012000 <arcex.29@gmail.com>
2022-06-20 16:09:28 +0200

    Milestone 1

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Array per i post
+const post = [
+    {
+        id: 1,
+        name: 'Phil Mangione',
+        profile: 'https://unsplash.it/300/300?image=15',
+        date: '06/04/2022',
+        content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        img: 'https://unsplash.it/600/300?image=3',
+        like: 80
+    },
+    {
+        id: 2,
+        name: 'Sofia Perlari',
+        profile: 'https://unsplash.it/300/300?image=21',
+        date: '06/18/2022',
+        content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        img: 'https://unsplash.it/600/300?image=1',
+        like: 113
+    },
+    {
+        id: 3,
+        name: 'Mario Rossi',
+        profile: 'https://unsplash.it/300/300?image=56',
+        date: '04/12/2021',
+        content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        img: null,
+        like: 100
+    },
+    {
+        id: 4,
+        name: 'Luca Verdi',
+        profile: 'https://unsplash.it/300/300?image=93',
+        date: '06/21/2022',
+        content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        img: 'https://unsplash.it/600/300?image=1',
+        like: 43
+    }
+]
+
